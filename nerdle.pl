0456d4a75cb5d9c6545ee65bf229a1212f29df34
Peter Ludemann <peter.ludemann@gmail.com>
2022-08-12 15:10:56 -0700

    WIP - play and display

--- nerdle.pl
+++ nerdle.pl
@@ -37,13 +37,26 @@ expr(MinMax, Ss) :-
     all_syms(AllSyms),
     include(no(MinMax), AllSyms, No),
     include(yes(MinMax), AllSyms, Yes),
-    length(Ss, 8),
+    puzzle(Ss),
     possible(MinMax, Yes, No, AllSyms, Possible),
     maplist(in(Possible), Ss),
+    valid_puzzle(Ss).
+
+valid_puzzle(Ss) :-
     append(LeftSs, [=|RightSs], Ss),
+    maplist(in([0,1,2,3,4,5,6,7,8,9]), RightSs),
+    \+ adjacent_ops(LeftSs),
     chars_term(LeftSs, Left),
     chars_term(RightSs, Right),
     catch(Left =:= Right, _, fail).
+
+adjacent_ops(Ds) :-
+    append(_, [D1,D2|_], Ds),
+    member(D1, [+,-,*,/,=]),
+    member(D2, [+,-,*,/,=]).
+
+puzzle(Ss) :-
+    length(Ss, 8).
 
 min(MinMax, D, Min) :- (Min,_) = MinMax.D.
 max(MinMax, D, Max) :- (_,Max) = MinMax.D.
@@ -53,17 +66,17 @@ yes(MinMax, D) :- (1,_) = MinMax.D.
 possible(MinMax, _Yes, No, Possible0, Possible) :-
     include(not_in(No), Possible0, Possible),
     % foldl(select, _Yes, Possible, _), % maplist(valid_count...) supersedes this
-    zero_counts(ZeroCounts),
+    fill_summary(0, ZeroCounts),
     foldl(count, Possible, ZeroCounts, Counts),
     dict_pairs(Counts, _, CountPairs),
     maplist(valid_count(MinMax), CountPairs).
 
-zero_counts(ZeroCounts) :-
+fill_summary(Fill, ZeroCounts) :-
     all_syms(AllSyms),
-    maplist(zero_count, AllSyms, ZeroCountsList),
+    maplist(zero_count(Fill), AllSyms, ZeroCountsList),
     dict_create(ZeroCounts, counts, ZeroCountsList).
 
-zero_count(D, D-0).
+zero_count(Fill, D, D-Fill).
 
 count(D, Counts0, Counts) :-
     C is Counts0.D + 1,
@@ -85,4 +98,75 @@ not_in(NotList, X) :-
 
 in(List, X) :-
     member(X, List).
+
+% make_puzzle/1 generates an infinite number of puzzles, using
+% backtracking (so, don't do bagof(S, make_puzzle(S), Ss) because it
+% won't terminate).
+make_puzzle(Ss) :-
+    all_syms(AllSyms),
+    puzzle(Ss),
+    repeat,
+    maplist(random_elem(AllSyms), Ss),
+    valid_puzzle(Ss).
+
+random_elem(List, Elem) :-
+    length(List, Length),
+    random(1, Length, I),
+    nth1(I, List, Elem).
+
+:- det(run_puzzle/0).
+run_puzzle :-
+    make_puzzle(Ss),
+    !, % don't backtrack into another puzzle
+    run_puzzle(Ss).
+
+:- det(run_puzzle/1).
+run_puzzle(Ss) =>
+    % Emacs: (ansi-color-for-comint-mode-on)
+    run_puzzle(Ss, []).
+
+:- det(run_puzzle/2).
+run_puzzle(Ss, Guesses) =>
+    read_guess(Guess),
+    append(Guesses, [Guess], Guesses2),
+    display_result(Guesses2, Ss),
+    run_puzzle2(Ss, Guess, Guesses).
+
+:- det(run_puzzle2/3).
+run_puzzle2(Ss, Guess, _Guesses), solved(Ss, Guess) => true.
+run_puzzle2(Ss, _Guess, Guesses) =>
+    run_puzzle(Ss, Guesses).
+
+:- det(display_result/2).
+display_result(Guesses, Ss) =>
+    set_prolog_flag(color_term, true),
+    fill_summary(unknown, Summary0),
+    display_result(Guesses, Ss, Summary0).
+
+:- det(display_result/3).
+display_result([], Ss, Summary) =>
+    dict_pairs(Summary, _, SummaryPairs),
+    join(SummaryPairs, display_result_summary, write(' ')),
+    format('~n', []).
+
+:- det(display_result_summary/1).
+display_result_summary(D-Type) =>
+    display_fmt(Type, Fmt),
+    ansi_format(Fmt, '~w', [D]).
+
+:- det(display_fmt/2).
+% Defined colors: black, red, green, yellow, blue, magenta, cyan, white
+display_fmt(unknown, Fmt) => Fmt = [bold, bg(cyan),    fg(black)].
+display_fmt(wrong,   Fmt) => Fmt = [bold, bg(black),   fg(white)].
+display_fmt(correct, Fmt) => Fmt = [bold, bg(green),   fg(white)].
+display_fmt(partial, Fmt) => Fmt = [bold, bg(magenta), fg(white)].
+
+:- det(join/3).
+join([], _, _) => true.
+join([X], Call, _) => call(Call, X).
+join([X|Xs], Call, Call2) =>
+    call(Call, X),
+    call(Call2),
+    join(Xs, Call, Call2).
+
 
